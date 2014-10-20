85662b14cb5126e5e6a66b80df85feacf13685a6
Keir Fraser <keir.xen@gmail.com>
2014-10-20 10:15:15 +0100

    Better stanza link to unlicense.org

--- console.c
+++ console.c
@@ -6,7 +6,7 @@
  * Written & released by Keir Fraser <keir.xen@gmail.com>
  * 
  * This is free and unencumbered software released into the public domain.
- * See the file COPYING for more details, or visit unlicense.org.
+ * See the file COPYING for more details, or visit <http://unlicense.org>.
  */
 
 #include "stm32f10x.h"
