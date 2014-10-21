4ceb4e77c6183f6de28ad4d4b2011b8ea0b173c6
Keir Fraser <keir.xen@gmail.com>
2014-10-21 21:38:16 +0100

    Fix LED driver serial protocol after taking measurements with a logic analyser.

--- led.c
+++ led.c
@@ -13,14 +13,37 @@
 #include "stm32f10x.h"
 #include "intrinsics.h"
 
+/* Brightness range is 0-7: 
+ * 0 is very dim
+ * 1-2 are easy on the eyes 
+ * 3-7 are varying degrees of retina burn */
+#define BRIGHTNESS 1
+
+/* Specified f_max is 500kHz with 50% duty cycle, so clock should change 
+ * change value no more often than 1us. Pick 2us here conservatively. 
+ * Actually we can clock much faster than this and, conversely, very much 
+ * slower too. */
+#define DELAY_US 2
+
+/* Serial bus */
 #define DAT 10 /* PB10 */
 #define CLK 11 /* PB11 */
-#define write_clk(val) gpio_write_pin(gpiob, CLK, val)
-#define write_dat(val) gpio_write_pin(gpiob, DAT, val)
+
+static void write_clk(unsigned int val)
+{
+    gpio_write_pin(gpiob, CLK, val);
+    delay_us(DELAY_US);
+}
+
+static void write_dat(unsigned int val)
+{
+    gpio_write_pin(gpiob, DAT, val);
+    delay_us(DELAY_US);
+}
 
 static const uint8_t digits[] = {
-    0x7e, 0x0c, 0xb6, 0x9e, 0xcc, 0xda, 0xfa, 0x0e, /* 0-7 */
-    0xfe, 0xde, 0xee, 0xf8, 0x72, 0xbc, 0xf2, 0xe2, /* 8-f */
+    0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, /* 0-7 */
+    0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, /* 8-f */
 };
 
 static void write(uint8_t x)
@@ -29,38 +52,25 @@ static void write(uint8_t x)
 
     /* Data, LSB first */
     for (i = 0; i < 8; i++) {
-        write_clk(0);
         write_dat(x&1);
         x >>= 1;
-        delay_us(2);
         write_clk(1);
-        delay_us(2);
+        write_clk(0);
     }
 
-    /* Wait for ACK from TM1651 */
-    write_clk(0);
-    write_dat(1);
+    write_dat(0);
     write_clk(1);
-    gpio_configure_pin(gpiob, DAT, GPI_pulled);
-    for (i = 0; (i < 5) && gpio_read_pin(gpiob, DAT); i++)
-        delay_us(1);
-
-    gpio_configure_pin(gpiob, DAT, GPO_pushpull);
+    write_clk(0);
 }
 
 static void start(void)
 {
-    write_clk(1);
-    write_dat(1);
-    delay_us(2);
     write_dat(0);
     write_clk(0);
-} 
+}
 
 static void stop(void)
 {
-    write_clk(0);
-    write_dat(0);
     write_clk(1);
     write_dat(1);
 }
@@ -78,15 +88,21 @@ void leds_write_hex(unsigned int x)
 
 void leds_init(void)
 {
+    /* Prepare the bus. */
     gpio_configure_pin(gpiob, DAT, GPO_pushpull);
     gpio_configure_pin(gpiob, CLK, GPO_pushpull);
-
-    start();
-    write(0x40); /* data cmd: write, auto-incr addr */
+    write_clk(0);
+    write_dat(0);
     stop();
 
+    /* Data command: write registers, auto-increment address. */
     start();
-    write(0x93); /* display ctrl: low brightness */
+    write(0x40);
+    stop();
+
+    /* Display control: low brightness. */
+    start();
+    write(0x88 + BRIGHTNESS);
     stop();
 }
 
