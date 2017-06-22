77e3e30a9307d2057f10ad366b59d0c7d94a3c45
Keir <keir.xen@gmail.com>
2017-06-22 17:56:42 +0100

    tests: Increase range of tests, add histogram-like reporting. Document some USB sticks I have to hand.

--- src/tests.c
+++ src/tests.c
@@ -16,6 +16,7 @@ extern FIL file;
  */
 
 struct stats {
+    unsigned int histo[15];
     unsigned int min_us, max_us;
     unsigned int tot_us;
     unsigned int nr_ops;
@@ -35,13 +36,35 @@ static void stats_update(struct stats *stats, unsigned int op_us)
     if (op_us < stats->min_us)
         stats->min_us = op_us;
     stats->tot_us += op_us;
+    if (op_us >= 500000)
+        stats->histo[14]++; /* 500ms+ */
+    else if (op_us >= 100000)
+        stats->histo[9+op_us/100000]++; /* 100-499ms */
+    else
+        stats->histo[op_us/10000]++; /* 0-99ms */
 }
 
 static void stats_print(struct stats *stats)
 {
-    printk("Min: %uus Max: %uus Mean: %uus\n",
+    unsigned int i, nr_long = 0;
+    printk("  Min: %uus Max: %uus Mean: %uus\n",
            stats->min_us, stats->max_us,
            stats->tot_us / stats->nr_ops);
+    printk("  Histo: ");
+    for (i = 0; i < 10; i++) {
+        if (stats->histo[i] == 0)
+            continue;
+        printk("%u-%ums:%u ", i*10, i*10+9, stats->histo[i]);
+    }
+    for (i = 0; i < 5; i++) {
+        if (stats->histo[10+i] == 0)
+            continue;
+        printk("%u-%ums:%u ", (i+1)*100, (i+2)*100-1, stats->histo[10+i]);
+    }
+    for (i = 5; i < 15; i++)
+        nr_long += stats->histo[i];
+    printk(">50ms: %u.%02u%%\n", (nr_long*100)/stats->nr_ops,
+           ((nr_long*10000)/stats->nr_ops)%100);
 }
 
 /*
@@ -106,11 +129,11 @@ static uint32_t random(uint32_t *rnd)
  * I/O Tests
  */
 
-#define TEST_MB 1
+#define TEST_MB 8
 #define TEST_SZ (TEST_MB*1024*1024)
 static void speed_subtests(FIL *fp, struct stats *stats,
                            char *buf, unsigned int bufsz,
-                           int do_write, unsigned int blksz)
+                           int do_write, int do_delay, unsigned int blksz)
 {
     unsigned int i, j;
     uint32_t t[2], rnd = 0x12345678;
@@ -126,11 +149,16 @@ static void speed_subtests(FIL *fp, struct stats *stats,
         t[1] = time_now();
         stats_update(stats, t[1]-t[0]);
         t[0] = t[1];
+        if (do_delay && !(~i&((bufsz/blksz)-1))) {
+            delay_ms(200);
+            t[0] = time_now();
+        }
     }
-    printk("Sequential %u-byte %ss (%uMB total):: ", blksz,
-           do_write ? "write" : "read", TEST_MB);
+    printk("%sSequential %u-byte %ss (%uMB total):\n",
+           do_delay ? "Delayed " : "",
+           blksz, do_write ? "write" : "read", TEST_MB);
     stats_print(stats);
-    
+
     stats_init(stats);
     t[0] = time_now();
     for (i = 0; i < TEST_SZ / blksz; i++) {
@@ -142,9 +170,14 @@ static void speed_subtests(FIL *fp, struct stats *stats,
         t[1] = time_now();
         stats_update(stats, t[1]-t[0]);
         t[0] = t[1];
+        if (do_delay && !(~i&((bufsz/blksz)-1))) {
+            delay_ms(200);
+            t[0] = time_now();
+        }
     }
-    printk("Random %u-byte %ss (%uMB total):: ", blksz,
-           do_write ? "write" : "read", TEST_MB);
+    printk("%sRandom %u-byte %ss (%uMB total):\n",
+           do_delay ? "Delayed " : "",
+           blksz, do_write ? "write" : "read", TEST_MB);
     stats_print(stats);
     
     if (blksz >= bufsz)
@@ -164,9 +197,14 @@ static void speed_subtests(FIL *fp, struct stats *stats,
             stats_update(stats, t[1]-t[0]);
             t[0] = t[1];
         }
+        if (do_delay) {
+            delay_ms(200);
+            t[0] = time_now();
+        }
     }
-    printk("Clustered Random %u-byte %ss (%uMB total):: ", blksz,
-           do_write ? "write" : "read", TEST_MB);
+    printk("%sClustered Random %u-byte %ss (%uMB total):\n",
+           do_delay ? "Delayed " : "",
+           blksz, do_write ? "write" : "read", TEST_MB);
     stats_print(stats);
 }
 
@@ -192,13 +230,15 @@ void speed_tests(void)
         stats_update(&stats, t[1]-t[0]);
         t[0] = t[1];
     }
-    printk("Sequential create (%uMB total, 8kB block size):: ", TEST_MB);
+    printk("Sequential create (%uMB total, 8kB block size):\n", TEST_MB);
     stats_print(&stats);
 
-    speed_subtests(&file, &stats, buf, sizeof(buf), 0, 512);
-    speed_subtests(&file, &stats, buf, sizeof(buf), 0, sizeof(buf));
-    speed_subtests(&file, &stats, buf, sizeof(buf), 1, 512);
-    speed_subtests(&file, &stats, buf, sizeof(buf), 1, sizeof(buf));
+    speed_subtests(&file, &stats, buf, sizeof(buf), 0, 0, 512);
+    speed_subtests(&file, &stats, buf, sizeof(buf), 0, 0, sizeof(buf));
+    speed_subtests(&file, &stats, buf, sizeof(buf), 1, 0, 512);
+    speed_subtests(&file, &stats, buf, sizeof(buf), 1, 0, sizeof(buf));
+    speed_subtests(&file, &stats, buf, sizeof(buf), 1, 1, 512);
+    speed_subtests(&file, &stats, buf, sizeof(buf), 1, 1, sizeof(buf));
 
     F_close(&file);
     F_unlink("speed_test");
