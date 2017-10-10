2d61f06d43c5975d7e4c1e487f90ac548738a072
Keir Fraser <keir.xen@gmail.com>
2017-10-10 21:10:40 +0100

    Update to v0.8.3a

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 PROJ = FlashFloppy
-VER = v0.8.2a
+VER = v0.8.3a
 
 SUBDIRS += src bootloader reloader
 
--- inc/util.h
+++ inc/util.h
@@ -9,7 +9,7 @@
  * See the file COPYING for more details, or visit <http://unlicense.org>.
  */
 
-#define FW_VER "0.8.2a"
+#define FW_VER "0.8.3a"
 
 #ifndef NDEBUG
 #define ASSERT(p) do { if (!(p)) illegal(); } while (0)
