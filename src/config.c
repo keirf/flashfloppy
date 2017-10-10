4d4a7767a07bf1f10b5d8da50ce6d5309401cf7d
Keir Fraser <keir.xen@gmail.com>
2017-10-10 20:04:21 +0100

    config: Accept quoted strings of arbitrary chars in option value.

--- src/config.c
+++ src/config.c
@@ -26,30 +26,51 @@ int get_next_opt(struct opts *opts)
 next_line:
     if (c == '\0')
         return -1; /* eof */
+    /* Skip leading whitespace. */
     while (isspace(c))
         F_read(opts->file, &c, 1, NULL);
+
+    /* Option name parsing. */
     p = opts->arg;
     while (isvalid(c) && ((p-opts->arg) < (opts->argmax-1))) {
         *p++ = c;
         F_read(opts->file, &c, 1, NULL);
     }
     *p = '\0';
+    /* Look for a match in the accepted options list. */
     for (opt = opts->opts; opt->name; opt++)
         if (!strcmp(opt->name, opts->arg))
             break;
     if (!opt->name) {
+        /* No match? Then skip to next line and try again. */
         while ((c != '\r') && (c != '\n') && (c != '\0'))
             F_read(opts->file, &c, 1, NULL);
         goto next_line;
     }
+
+    /* Skip whitespace (and equals) between option name and value. */
     while ((c == ' ') || (c == '\t') || (c == '='))
         F_read(opts->file, &c, 1, NULL);
+
+    /* Option value parsing: */
     p = opts->arg;
-    while (isvalid(c) && ((p-opts->arg) < (opts->argmax-1))) {
-        *p++ = c;
+    if (c == '"') {
+        /* Arbitrary quoted string. Anything goes except NL/CR. */
         F_read(opts->file, &c, 1, NULL);
+        while ((c != '\r') && (c != '\n') && (c != '"') && (c != '\0')
+               && ((p-opts->arg) < (opts->argmax-1))) {
+            *p++ = c;
+            F_read(opts->file, &c, 1, NULL);
+        }
+    } else {
+        /* Non-quoted value: pretty much alphanumeric only is accepted. */
+        while (isvalid(c) && ((p-opts->arg) < (opts->argmax-1))) {
+            *p++ = c;
+            F_read(opts->file, &c, 1, NULL);
+        }
     }
     *p = '\0';
+
     return opt - opts->opts;
 }
 
