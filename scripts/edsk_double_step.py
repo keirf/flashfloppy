fc181422e6f1e052d3e17e81483e8142e5c507e1
Keir Fraser <keir.xen@gmail.com>
2020-02-16 22:57:39 +0000

    edsk: Fix double-step script to support Extended CPC DSK images. Refs #318

--- scripts/edsk_double_step.py
+++ scripts/edsk_double_step.py
@@ -22,19 +22,33 @@ def main(argv):
         return
     # Check image header
     sig, _, tracks, sides, tsz = struct.unpack("<34s14sBBH", in_dat[:52])
-    if not sig.startswith(b"MV - CPCEMU"):
+    out[48] = tracks * 2 # double up on number of cyls
+    tszs = in_dat[52:256]
+    in_dat = in_dat[256:]
+    if sig.startswith(b"MV - CPCEMU"):
+        for i in range(tracks):
+            out += in_dat[:tsz*sides]
+            for j in range(sides):
+                out[16-tsz*(j+1)] = i*2 # fix cyl#
+            out += in_dat[:tsz*sides]
+            for j in range(sides):
+                out[16-tsz*(j+1)] = i*2+1 # fix cyl#
+            in_dat = in_dat[tsz*sides:]
+    elif sig.startswith(b"EXTENDED CPC DSK File\r\nDisk-Info\r\n"):
+        for i in range(tracks):
+            for j in range(2):
+                off = 0
+                for k in range(sides):
+                    tsz = tszs[k]*256
+                    out += in_dat[off:off+tsz]
+                    out[16-tsz] = i*2+j # fix cyl#
+                    out[52+(i*2+j)*sides+k] = tszs[k] # fix track size
+                    off += tsz
+            tszs = tszs[sides:]
+            in_dat = in_dat[off:]
+    else:
         print("Not a valid EDSK image")
         return
-    out[48] = tracks * 2 # souble up on number of cyls
-    in_dat = in_dat[256:]
-    for i in range(tracks):
-        out += in_dat[:tsz*sides]
-        for j in range(sides):
-            out[16-tsz*(j+1)] = i*2 # fix cyl#
-        out += in_dat[:tsz*sides]
-        for j in range(sides):
-            out[16-tsz*(j+1)] = i*2+1 # fix cyl#
-        in_dat = in_dat[tsz*sides:]
     with open(argv[2], "wb") as f:
         f.write(out)
     
