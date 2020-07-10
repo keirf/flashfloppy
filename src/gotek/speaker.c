0551750868187ff788d31e89ac516a2c6fbbea39
Keir Fraser <keir.xen@gmail.com>
2020-07-10 14:56:12 +0100

    speaker: Rate-limit speaker pulses to one per 2.7ms. Avoids annoying high-frequency chirp when entering D-A mode or in Gotek-aware high-speed loaders.

--- src/gotek/speaker.c
+++ src/gotek/speaker.c
@@ -13,25 +13,50 @@
 #define gpio_spk gpioa
 #define pin_spk 2
 
-static struct timer pulse_timer;
+static struct {
+    volatile enum { STATE_idle, STATE_active, STATE_masked } state;
+    time_t start;
+    struct timer timer;
+} pulse;
+
 static void pulse_timer_fn(void *unused)
 {
-    gpio_write_pin(gpio_spk, pin_spk, FALSE);
+    switch (pulse.state) {
+    case STATE_idle:
+        break;
+    case STATE_active:
+        gpio_write_pin(gpio_spk, pin_spk, FALSE);
+        pulse.state = STATE_masked;
+        /* Mask to typical minimum floppy step cycle (3ms) less 10% */
+        timer_set(&pulse.timer, pulse.start + time_us(2700));
+        break;
+    case STATE_masked:
+        pulse.state = STATE_idle;
+        break;
+    }
 }
 
 void speaker_init(void)
 {
+    pulse.state = STATE_idle;
     gpio_configure_pin(gpio_spk, pin_spk, GPO_pushpull(_2MHz, FALSE));
-    timer_init(&pulse_timer, pulse_timer_fn, NULL);
+    timer_init(&pulse.timer, pulse_timer_fn, NULL);
 }
 
 void speaker_pulse(void)
 {
     unsigned int volume = ff_cfg.step_volume;
-    if (!volume)
+    time_t now;
+
+    if (!volume || (pulse.state != STATE_idle))
         return;
+
     gpio_write_pin(gpio_spk, pin_spk, TRUE);
-    timer_set(&pulse_timer, time_now() + volume*volume*3);
+
+    now = time_now();
+    pulse.state = STATE_active;
+    pulse.start = now;
+    timer_set(&pulse.timer, now + volume*volume*3);
 }
 
 /*
