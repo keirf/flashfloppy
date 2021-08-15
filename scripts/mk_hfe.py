767768039e4ef7c2221545a8597d46b37af9bb88
Eric Anderson <ejona86@gmail.com>
2021-08-14 17:08:52 -0700

    mk_hfe.py: Add hard sector support using HFEv3
    The 16-byte boundary's purpose was unclear to me, but it seems
    unnecessary with HFEv3.

--- scripts/mk_hfe.py
+++ scripts/mk_hfe.py
@@ -20,14 +20,22 @@ def main(argv):
                       help="number of cylinders")
   parser.add_argument("--sides", type=int, default=2,
                       help="number of sides")
+  parser.add_argument("--hard-sectors", type=int, default=0,
+                      help="number of hard sectors")
   parser.add_argument("outfile", help="output filename")
   args = parser.parse_args(argv[1:])
+
+  v3 = args.hard_sectors != 0
 
   bits = (args.rate * 1000 * 60) // args.rpm
   bits *= 2 # clock bits
   bits *= 2 # 2 sides
-  bytes = (bits + 7) // 8     # convert to bytes, rounded up
-  bytes = (bytes + 15) & ~15  # round up to 16-byte boundary
+  bytes = (bits + 7) // 8 # convert to bytes, rounded up
+  if not v3:
+    bytes = (bytes + 15) & ~15 # round up to 16-byte boundary
+  raw_bytes = bytes
+  if args.hard_sectors:
+    bytes += (args.hard_sectors + 1) * 2 # 2 sides
   blocks = (bytes + 511) // 512 # convert to 512-byte blocks, rounded up
 
   print("Geometry: %u cylinders, %u sides" % (args.cyls, args.sides))
@@ -37,8 +45,9 @@ def main(argv):
   
   # Header
   out_f = open(args.outfile, "wb")
+  sig = b'HXCHFEV3' if v3 else b"HXCPICFE"
   out_f.write(struct.pack("<8s4B2H2BH",
-                          b"HXCPICFE",# signature
+                          sig,        # signature
                           0,          # revision
                           args.cyls,  # nr_tracks
                           args.sides, # nr_sides
@@ -59,7 +68,24 @@ def main(argv):
   out_f.write(bytearray(b'\xff'*(tlut_blocks*512-args.cyls*4)))
 
   # Data
-  out_f.write(bytearray(b'\x88'*(blocks*512*args.cyls)))
+  if not args.hard_sectors:
+    out_f.write(bytearray(b'\x88'*(blocks*512*args.cyls)))
+  else:
+    trk_side = bytearray(b'\x88')*(raw_bytes//2)
+    sector_len = float(raw_bytes//2) / args.hard_sectors
+    trk_side.insert(-int(sector_len)//2, 0x8F)
+    for sector in range(args.hard_sectors-1, -1, -1):
+      trk_side.insert(int(sector_len*sector), 0x8F)
+    assert len(trk_side) == bytes//2
+    trk_side += b'\x0F'*(256 - len(trk_side)%256)
+    assert len(trk_side) == blocks*256
+
+    trk = bytearray()
+    for pos in range(0, len(trk_side), 256):
+      trk.extend(trk_side[pos:pos+256])
+      trk.extend(trk_side[pos:pos+256])
+    for _ in range(args.cyls):
+      out_f.write(trk)
     
 if __name__ == "__main__":
     main(sys.argv)
