keirf/flashfloppy

commit 991a7747e5750a13b3abbadc88a4e6911b7bf271

Add hard sector support to infrastructure and HFE

src/floppy_generic.c
@@ -78,8 +78,13 @@ static struct image *image;
 
 static struct {
     struct timer timer, timer_deassert;
+    struct timer custom_timer;
     time_t prev_time;
     bool_t fake_fired;
+    uint8_t custom_pulses_ver;
+    uint8_t custom_pulses_len;
+    /* Durations relative to track start. Must be in increasing order. */
+    time_t custom_pulses[MAX_CUSTOM_PULSES];
 } index;
 
 static unsigned int drive_calc_track(struct drive *drv);
@@ -268,6 +273,8 @@ static void floppy_mount(struct slot *slot)
 
     drv->index_suppressed = FALSE;
     index.prev_time = time_now();
+    index.custom_pulses_len = 0;
+    index.custom_pulses_ver = 0xFF;
 }
 
 /* Initialise timers and DMA for RDATA/WDATA. */
@@ -636,6 +643,28 @@ static void IRQ_rdata_dma(void)
     } else if (nr != nr_to_cons) {
         /* We didn't fill the ring: re-enter this ISR to do more work. */
         IRQx_set_pending(dma_rdata_irq);
+    }
+
+    ASSERT(drv->image->index_pulses_len < MAX_CUSTOM_PULSES);
+    if (drv->image->index_pulses_ver != index.custom_pulses_ver) {
+        time_t current_pulse_pos = time_since(index.prev_time);
+        uint32_t oldpri;
+
+        oldpri = IRQ_save(TIMER_IRQ_PRI);
+        for (i = 0; i < drv->image->index_pulses_len; i++)
+            index.custom_pulses[i] = (drv->image->index_pulses[i]>>4)
+                / (SYSCLK_MHZ/TIME_MHZ);
+        index.custom_pulses_len = drv->image->index_pulses_len;
+        index.custom_pulses_ver = drv->image->index_pulses_ver;
+
+        for (i = 0; i < index.custom_pulses_len; i++)
+            if (current_pulse_pos <= index.custom_pulses[i])
+                break;
+        if (i < index.custom_pulses_len)
+            timer_set(&index.custom_timer, index.prev_time + index.custom_pulses[i]);
+        else
+            timer_cancel(&index.custom_timer);
+        IRQ_restore(oldpri);
     }
 
     /* Check if we have crossed the index mark. If not, we're done. */

inc/floppy.h
@@ -72,6 +72,7 @@ struct hfe_image {
     uint16_t tlut_base;
     uint16_t trk_pos, trk_len;
     bool_t is_v3, double_step, fresh_seek;
+    uint8_t next_index_pulses_pos;
 };
 
 struct qd_image {
@@ -165,6 +166,8 @@ struct directaccess {
     bool_t read_op_started;
 };
 
+#define MAX_CUSTOM_PULSES 34 /* 33+1 for minor track misalignment */
+
 struct image {
     /* Handler for currently-selected type of disk image. */
     const struct image_handler *disk_handler;
@@ -177,6 +180,11 @@ struct image {
 
     /* Info about image as a whole. */
     uint8_t nr_cyls, nr_sides;
+    uint8_t index_pulses_ver; /* Changed when pulses or length changed. */
+    uint8_t index_pulses_len;
+    /* Alternative index pulse locations, in ticks. When non-empty, disables
+     * regular index pulse. */
+    uint32_t index_pulses[MAX_CUSTOM_PULSES];
 
     /* Data buffers. */
     struct image_bufs bufs;

src/floppy.c
@@ -26,6 +26,7 @@ void IRQ_43(void) __attribute__((alias("IRQ_soft")));
 /* Index-pulse timer functions. */
 static void index_assert(void *);   /* index.timer */
 static void index_deassert(void *); /* index.timer_deassert */
+static void index_custom_assert(void *);   /* index.custom_timer */
 
 static time_t sync_time, sync_pos;
 
@@ -180,6 +181,7 @@ void floppy_cancel(void)
     /* Clear soft state. */
     timer_cancel(&drv->chgrst_timer);
     timer_cancel(&index.timer);
+    timer_cancel(&index.custom_timer);
     barrier(); /* cancel index.timer /then/ clear dma rings */
     dma_rd = dma_wr = NULL;
     barrier(); /* /then/ clear soft state */
@@ -304,6 +306,7 @@ void floppy_init(void)
 
     timer_init(&index.timer, index_assert, NULL);
     timer_init(&index.timer_deassert, index_deassert, NULL);
+    timer_init(&index.custom_timer, index_custom_assert, NULL);
 
     motor_chgrst_eject(drv);
 }
@@ -414,6 +417,7 @@ static void floppy_sync_flux(void)
         uint32_t oldpri = IRQ_save(TIMER_IRQ_PRI);
 
         timer_cancel(&index.timer);
+        timer_cancel(&index.custom_timer);
 
         /* If we crossed the index mark while filling the DMA buffer then we
          * need to set up the index pulse (usually done by IRQ_rdata_dma). */
@@ -432,6 +436,17 @@ static void floppy_sync_flux(void)
             /* Calculate deadline for index timer. */
             ticks /= SYSCLK_MHZ/TIME_MHZ;
             timer_set(&index.timer, time_now() + ticks);
+        }
+
+        /* Realign custom_timer to index. */
+        if (index.custom_pulses_len) {
+            int i;
+            for (i = 0; i < index.custom_pulses_len; i++)
+                if (sync_pos < index.custom_pulses[i])
+                    break;
+            if (i < index.custom_pulses_len)
+                timer_set(&index.custom_timer,
+                          time_now() - sync_pos + index.custom_pulses[i]);
         }
 
         IRQ_global_disable();
@@ -562,10 +577,13 @@ static void index_assert(void *dat)
 {
     struct drive *drv = &drive;
     index.prev_time = index.timer.deadline;
-    if (drv->motor.on && !index_is_suppressed(drv)) {
-        drive_change_output(drv, outp_index, TRUE);
-        timer_set(&index.timer_deassert, index.prev_time + time_ms(2));
-    }
+    if (index.custom_pulses_len)
+        timer_set(&index.custom_timer, index.prev_time + index.custom_pulses[0]);
+    else
+        if (drv->motor.on && !index_is_suppressed(drv)) {
+            drive_change_output(drv, outp_index, TRUE);
+            timer_set(&index.timer_deassert, index.prev_time + time_ms(2));
+        }
     if (dma_rd->state != DMA_active) /* timer set from input flux stream */
         timer_set(&index.timer, index.prev_time + drv->image->stk_per_rev);
 }
@@ -574,6 +592,24 @@ static void index_deassert(void *dat)
 {
     struct drive *drv = &drive;
     drive_change_output(drv, outp_index, FALSE);
+}
+
+static void index_custom_assert(void *dat)
+{
+    struct drive *drv = &drive;
+    time_t current_pulse_pos;
+    int i;
+    if (drv->motor.on && !index_is_suppressed(drv)) {
+        drive_change_output(drv, outp_index, TRUE);
+        timer_set(&index.timer_deassert,
+                  index.custom_timer.deadline + time_ms(2));
+    }
+    current_pulse_pos = time_since(index.prev_time);
+    for (i = 0; i < index.custom_pulses_len; i++)
+        if (current_pulse_pos < index.custom_pulses[i])
+            break;
+    if (i < index.custom_pulses_len)
+        timer_set(&index.custom_timer, index.prev_time + index.custom_pulses[i]);
 }
 
 static void chgrst_timer(void *_drv)

src/image/hfe.c
@@ -157,6 +157,7 @@ static void hfe_setup_track(
     uint32_t sys_ticks, opcode_adj_bc = 0;
     uint8_t cyl = track >> (im->hfe.double_step ? 2 : 1);
     uint8_t side = track & (im->nr_sides - 1);
+    int i;
 
     track = cyl*2 + side;
     if (track/2 != im->cur_track/2) {
@@ -211,6 +212,11 @@ static void hfe_setup_track(
     sys_ticks = im->cur_ticks / 16;
 
     bc->prod = bc->cons = 0;
+
+    for (i = 0; i < im->index_pulses_len; i++)
+        if (im->cur_ticks < im->index_pulses[i])
+            break;
+    im->hfe.next_index_pulses_pos = i;
 
     if (start_pos) {
         /* Read mode. */
@@ -288,6 +294,11 @@ static uint16_t hfe_rdata_flux(struct image *im, uint16_t *tbuf, uint16_t nr)
             im->cur_bc = im->cur_ticks = 0;
             /* Skip tail of current 256-byte block. */
             bc_c = (bc_c + 256*8-1) & ~(256*8-1);
+            if (im->index_pulses_len != im->hfe.next_index_pulses_pos) {
+                im->index_pulses_len = im->hfe.next_index_pulses_pos;
+                im->index_pulses_ver++;
+            }
+            im->hfe.next_index_pulses_pos = 0;
             continue;
         }
         y = bc_c % 8;
@@ -295,8 +306,19 @@ static uint16_t hfe_rdata_flux(struct image *im, uint16_t *tbuf, uint16_t nr)
         if (is_v3 && (y == 0) && ((x & 0xf) == 0xf)) {
             /* V3 byte-aligned opcode processing. */
             switch (x >> 4) {
-            case OP_nop:
             case OP_index:
+                if (im->hfe.next_index_pulses_pos < MAX_CUSTOM_PULSES
+                    && im->index_pulses[im->hfe.next_index_pulses_pos] != im->cur_ticks) {
+
+                    im->index_pulses[im->hfe.next_index_pulses_pos]
+                        = im->cur_ticks;
+                    if (im->index_pulses_len < im->hfe.next_index_pulses_pos+1)
+                        im->index_pulses_len = im->hfe.next_index_pulses_pos+1;
+                    im->index_pulses_ver++;
+                }
+                im->hfe.next_index_pulses_pos++;
+                /* fallthrough */
+            case OP_nop:
             default:
                 bc_c += 8;
                 im->cur_bc += 8;

commit 1eeb28fa49529ad0cd918ee64f3328637a5de382

Fix hard sector fallout for quickdisk
991a7747 accidentally broke quickdisk, as quickdisk doesn't use the
timers involved. Just exclude the code that isn't needed for quickdisk.

src/floppy_generic.c
@@ -645,6 +645,7 @@ static void IRQ_rdata_dma(void)
         IRQx_set_pending(dma_rdata_irq);
     }
 
+#if !defined(QUICKDISK)
     ASSERT(drv->image->index_pulses_len < MAX_CUSTOM_PULSES);
     if (drv->image->index_pulses_ver != index.custom_pulses_ver) {
         time_t current_pulse_pos = time_since(index.prev_time);
@@ -666,6 +667,7 @@ static void IRQ_rdata_dma(void)
             timer_cancel(&index.custom_timer);
         IRQ_restore(oldpri);
     }
+#endif
 
     /* Check if we have crossed the index mark. If not, we're done. */
     if (image_ticks_since_index(drv->image) >= prev_ticks_since_index)